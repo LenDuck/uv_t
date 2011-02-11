84cecb16e0c61fb1001724ac59189bc84dabc521
LenDuck <leen@nanoByte.(none)>
2011-02-11 15:25:44 +0100

    asda

--- src_server/main.c
+++ src_server/main.c
@@ -8,6 +8,8 @@
 #include "dlogger.h"
 #include "server.h"
 
+#define WELCOME "Phasers do not include batteries,\nPhaser don't even include tangibility\r\n"
+
 global_state_t global_null(void){
   global_state_t ret;
 
@@ -19,23 +21,53 @@ global_state_t global_null(void){
   ret.outgoing_thread_state = THREAD_STATE_DEAD;
   return ret;
 }
-/*unsigned int sleep(unsigned int secs) */
-int outgoing_subrun(global_state_t *state){
-  
-  if (!state) return CON_ERROR_UNKNOWN;
+void * global_spim(void * par){
+  global_state_t *state = (global_state_t *)par;
+  while (/*insert spimming code here*/ global_send_all(state, "Want cookie? give me money!\n\r")){
+    sleep(5);
+  }
+  return NULL;
+}
 
-  /*insert spimming code here*/
-  global_send_all(state, "Want cookie? give me money!\n\r");
-  sleep(5);
+/*unsigned int sleep(unsigned int secs) */
+int listen_subrun(global_state_t *state){
+  int rva = 0;
+  client_state_t *new_client = NULL; 
+  if (!state) return CON_ERROR_UNKNOWN;
+  new_client = malloc(sizeof(client_state_t));
+ 
+  if (!new_client) return CON_ERROR_MEMORY;
+  *new_client = client_null();
+  new_client->log = dlog_newlog_file("client log",
+                                "client.log",
+                                DLOG_FLAG_FFLUSH | DLOG_FLAG_NLINE);
+  rva = con_serve_accept(state->listen_connection, &new_client->connection);
+  if (new_client->connection){
+    con_send_line(new_client->connection,
+                  WELCOME);
+  } else {
+    fprintf(stderr,"Yikes %d\n",rva);
+  }
+  pthread_mutex_lock(&new_client->access);
+  new_client->thread_state = THREAD_STATE_RUNNING;
+  rva = pthread_create(&new_client->handler_thread, NULL, client_handler, (void*) new_client);
+  if (rva) perror("oops, thread");
+  pthread_mutex_unlock(&new_client->access);
+
   return CON_ERROR_NONE;
 }
 
-void *outgoing_handler(void *data){
+void *listen_handler(void *data){
   global_state_t *state = (global_state_t *)data;
   if (!state) return NULL;
-  
-  while (state->outgoing_thread_state == THREAD_STATE_RUNNING){
-    int rva = outgoing_subrun(state);
+ 
+  if (con_init_serve(state->listen_connection)){
+    dlog_log_text("serve fail",state->log);
+    return NULL;
+  }
+
+  while (state->listen_thread_state == THREAD_STATE_RUNNING){
+    int rva = listen_subrun(state);
     if (rva == CON_ERROR_NONE) continue;
     break;
   }
@@ -53,8 +85,6 @@ int main(int argc, char **argv) {
   state = malloc(sizeof(global_state_t));
   *state = global_null();
   
-  new_client = malloc(sizeof(client_state_t));
-  *new_client = client_null();
 
   state->log = dlog_newlog_file("Server log",
                                       "server.log",
@@ -67,29 +97,14 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  if (con_init_serve(state->listen_connection)){
-    dlog_log_text("serve fail",state->log);
-    return -1;
-  }
 
-
-  new_client->log = dlog_newlog_file("client log",
-                                "client.log",
-                                DLOG_FLAG_FFLUSH | DLOG_FLAG_NLINE);
-  rva = con_serve_accept(state->listen_connection, &new_client->connection);
-  if (new_client->connection){
-    con_send_line(new_client->connection,
-"Phasers do not include batteries,\nPhaser don't even include tangibility\n");
-  } else {
-    fprintf(stderr,"Yikes %d\n",rva);
-  }
-  pthread_mutex_lock(&new_client->access);
-  new_client->thread_state = THREAD_STATE_RUNNING;
-  rva = pthread_create(&new_client->handler_thread, NULL, client_handler, (void*) new_client);
+  pthread_mutex_lock(&state->access);
+  state->listen_thread_state = THREAD_STATE_RUNNING;
+  rva = pthread_create(&state->listen_thread, NULL, listen_handler, (void*) state);
   if (rva) perror("oops, thread");
-  pthread_mutex_unlock(&new_client->access);
+  pthread_mutex_unlock(&state->access);
 
-  pthread_join(new_client->handler_thread,NULL);
+  pthread_join(state->listen_thread,NULL);
   con_close(state->listen_connection);
   //con_close(new_client->connection);
 

ac48adbb1c59576e7fdcc420f3f61c045092c426
debian <robert@gigahard.nl>
2011-02-20 15:21:23 +0100

    Jemo Eder

---
+++ src_server/types.h
@@ -0,0 +1,77 @@
+#ifndef H_TYPES
+#define H_TYPES
+
+typedef struct struct_client_state{
+  int state;
+  int thread_state;
+  pthread_mutex_t access;
+  pthread_t handler_thread;
+
+  con_t connection;
+  dlog_t *log;
+  char *username;
+  char *current_line;
+
+  struct struct_global_state *global;  
+} client_state_t;
+
+typedef struct struct_client_list{
+  struct struct_client_list *next;
+  client_state_t *current; 
+} client_list_t;
+
+typedef struct struct_global_state{
+  client_list_t *clients;
+  pthread_mutex_t access;
+
+  int users_online;
+  con_t listen_connection;
+  dlog_t *log;
+  int listen_thread_state;
+  int outgoing_thread_state;
+  pthread_t listen_thread;
+  pthread_t outgoing_thread;
+} global_state_t;
+
+typedef enum {
+  CLIENT_STATE_CONNECTED = 1,
+  CLIENT_STATE_LOGGED_IN = 2,
+  CLIENT_STATE_NOT_ME = 4,
+  CLIENT_STATE_ONLY_ME = 8
+} enum_client_state;
+
+typedef enum {
+  SERVER_STATE_CONNECTED = 1,
+  SERVER_STATE_OPEN = 2,
+  SERVER_STATE_FULL = 3
+} enum_server_state;
+
+typedef enum {
+  MSG_TYPE_CHAT,
+  MSG_TYPE_USER,
+  MSG_TYPE_NAMES,
+  MSG_TYPE_SAY,
+  MSG_TYPE_OK,
+  MSG_TYPE_IN_USE,
+  MSG_TYPE_JOIN,
+  MSG_TYPE_LEAVE,
+  MSG_TYPE_RENAME
+} enum_msg_type;
+
+typedef enum {
+  STATUS_POS,
+  STATUS_NEG
+} enum_status;
+
+typedef struct {
+  char *arg;
+  enum_status status;
+  enum_msg_type msg_type;
+  enum_client_state to_whom;
+} msg_t;
+
+typedef struct {
+  char *command, *msg;
+} cmd_t;
+
+#endif
